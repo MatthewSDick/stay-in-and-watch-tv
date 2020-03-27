69dd6a3d709def79f6dbabd9d7b6f87e4bdb9376
MatthewSDick <matthew.dick@outlook.com>
2020-03-27 12:07:23 -0400

    Fix a small issue

--- src/pages/ShowDetailsPage.jsx
+++ src/pages/ShowDetailsPage.jsx
@@ -5,22 +5,23 @@ const ShowDetailsPage = (props) => {
   const showID = props.match.params.showID
 
   const [details, setDetails] = useState([])
-  const [summary, setSummary] = useState([])
+  const [summary, setSummary] = useState({})
 
   const getShowDetails = async () => {
     const detailsResp = await axios.get(
       `https://api.themoviedb.org/3/tv/${showID}/credits?api_key=e39bd4d7934850f869dcfd33c094d2bc&language=en-US`
     )
     setDetails(detailsResp.data.cast)
-    console.log(detailsResp)
+    console.log('details' + detailsResp)
   }
 
   const getShowSummary = async () => {
+    console.log(showID)
     const summaryResp = await axios.get(
-      `http://api.themoviedb.org/3/tv/${showID}?api_key=e39bd4d7934850f869dcfd33c094d2bc&language=en-US&page=1`
+      `https://api.themoviedb.org/3/tv/${showID}?api_key=e39bd4d7934850f869dcfd33c094d2bc&language=en-US&page=1`
     )
     setSummary(summaryResp.data)
-    console.log(summaryResp)
+    console.log(summaryResp.data)
   }
 
   useEffect(() => {
@@ -36,7 +37,8 @@ const ShowDetailsPage = (props) => {
           <div className="detail-box">
             <div className="left-box">
               <img
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${summary.poster_path}`}
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${summary.backdrop_path}`}
+                // src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${poster_path}`}
                 alt="Movie Poster"
               />
             </div>
@@ -46,7 +48,7 @@ const ShowDetailsPage = (props) => {
                 {summary.overview}
               </p>
               <p className="popularity">
-                <span style={{ color: 'white' }}>Popularity:</span>
+                <span style={{ color: 'white' }}>Popularity: </span>
 
                 {summary.popularity}
               </p>
